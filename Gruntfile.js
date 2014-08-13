bb9d527fa6770497144750b94b8a525ddf01d294
huanhuan <469973264@qq.com>
2014-08-13 23:14:46 +0800

    delete files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,72 +1,71 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         jshint: {
             options: {},
-            files: ['Gruntfile.js', 'src/js/**/*.js']
+            files: ['Gruntfile.js', 'build/js/**/*.js']
         },
-        concat: {
-            options: {
-                separator: ''
-            },
-            dist: {
-                src: ['src/js/**/*.js'],
-                dest: 'dest/js/<%= pkg.name %>.js'
-            }
-        },
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-            },
-            dist: {
-                files: {
-                    'dest/js/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
-                }
-            }
-        },
+        // concat: {
+        //     options: {
+        //         separator: ''
+        //     },
+        //     dist: {
+        //         src: ['build/js/**/*.js'],
+        //         dest: 'js/<%= pkg.name %>.js'
+        //     }
+        // },
+        // uglify: {
+        //     options: {
+        //         banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+        //     },
+        //     dist: {
+        //         files: {
+        //             'js/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        //         }
+        //     }
+        // },
         sass: {
             base: {
                 options: {
-                    banner: "/*font end base.css 2014-07-16*/",
-                    style: 'compressed',
-                    sourcemap: true
+                    banner: "/*font end base.css*/",
+                    compass: true
                 },
                 files: {
-                    'dest/css/base.min.css': 'src/sass/base.scss',
-                    'dest/css/<%= pkg.name %>.min.css': 'src/sass/<%= pkg.name %>.scss'
+                    'css/base.min.css': 'build/sass/base.scss',
+                    'css/<%= pkg.name %>.min.css': 'build/sass/<%= pkg.name %>.scss'
                 }
             }
         },
         jade: {
             compile: {
                 files: {
-                    'dest/page/index.html': 'src/page/index.jade'
+                    'index.html': 'build/page/index.jade'
                 }
             }
         },
         watch: {
             sass: {
-                files: ['src/sass/**/*.scss'],
+                files: ['build/sass/**/*.scss'],
                 tasks: ['sass']
             },
             jade: {
-                files: ['src/page/**/*.jade'],
-                tasks: ['jade']
+                 files: ['build/page/**/*.jade'],
+                 tasks: ['jade']
             }
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-uglify');
+    // grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-concat');
+    // grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-jade');
 
     //grunt.loadNpmTasks('grunt-contrib-qunit');
     //grunt.loadNpmTasks('grunt-contrib-copy');
     //grunt.registerTask('test', ['jshint', 'qunit']);
 
-    grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'sass', 'watch', 'jade']);
+    grunt.registerTask('default', ['jshint', 'sass', 'jade', 'watch']);
 
 };
